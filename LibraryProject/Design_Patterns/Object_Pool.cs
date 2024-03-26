1e0ab4abf6296e7817cd3023b959c785f6052bae
Ludning <94681711+Ludning@users.noreply.github.com>
2024-03-26 18:04:13 +0900

    싱글톤 업데이트

--- LibraryProject/Design_Patterns/Object_Pool.cs
+++ LibraryProject/Design_Patterns/Object_Pool.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,55 @@ using System.Threading.Tasks;
 
 namespace LibraryProject.Design_Patterns
 {
+    public enum GameObjectType
+    {
+        Bullet,
+        Monster,
+    }
+    public class GameObject
+    {
+        string name;
+        public void DeActivate()
+        {
+
+        }
+    }
     internal class Object_Pool
     {
+        Dictionary<GameObjectType, Stack<GameObject>> _pool = new Dictionary<GameObjectType, Stack<GameObject>>();
+
+        public GameObject GetObject(GameObjectType type)
+        {
+            CreatePool(type);
+            if (_pool.TryGetValue(type, out var obj))
+            {
+                return obj.Pop();
+            }
+            return null;
+        }
+        public void PushObject(GameObjectType type, GameObject gameObject)
+        {
+            gameObject.DeActivate();
+            if (_pool.TryGetValue(type, out var obj))
+            {
+                obj.Push(gameObject);
+            }
+        }
+
+        public void CreatePool(GameObjectType type, int count = 10)
+        {
+            if (_pool.ContainsKey(type))
+                return;
+
+            Stack<GameObject> temp = new Stack<GameObject>();
+            GameObject gameObject;
+            for (int i = 0; i < count; i++)
+            {
+                gameObject = new GameObject();
+                gameObject.DeActivate();
+                temp.Push(gameObject);
+            }
+            _pool.Add(type, temp);
+        }
     }
 }
---
+++ LibraryProject/Design_Patterns/Singleton_Pattern.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryProject.Design_Patterns
+{
+    /// <summary>
+    /// 싱글톤 패턴
+    /// 무조건 하나의 인스턴스만 존재하도록 하는 설계
+    /// 프로그램의 심장을 설계하는데 좋음, 두개의 인스턴스가 발생하는 실수를 방지할 수 있기 때문
+    /// 동일하게 하나만 필요하고 게임이 끝나기 전까지 해제하지 않을 객체가 있으면
+    /// 동적으로 객체를 생성하고 프로퍼티로 Instance.example;로 가져가도록 한다.
+    /// </summary>
+    internal class Singleton_Pattern
+    {
+        private static Singleton_Pattern _instance;
+
+        public static Singleton_Pattern Instance
+        {
+            get
+            {
+                if(_instance == null)
+                    _instance = new Singleton_Pattern();
+                return _instance;
+            }
+        }
+
+        private ExampleManager example = new ExampleManager();
+        public static ExampleManager Example => Instance.example;
+    }
+
+    public class ExampleManager()
+    {
+
+    }
+}
